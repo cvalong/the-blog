de7bcc3ba4eb3469f0cc9aa363f7e0af1da91754
Christopher Long <93620761+cvalong@users.noreply.github.com>
2024-08-08 17:07:05 -0700

    Blog/routing (#1)
    * feat: add blog route
    
    * feat: add initial page setup

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,9 @@
+export default function BlogPage() {
+  return (
+    <main className='bg-[#f3f0e8] dark:bg-[#18251D] flex flex-col justify-start md:grid md:grid-cols-[1fr_640px_1fr]'>
+      <div className='space-y-4 py-10 px-6 md:col-start-2'>
+        <p>Hello From Next.js</p>
+      </div>
+    </main>
+  );
+}
