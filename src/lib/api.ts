550f1429245c972fd47e5691c30e6b2ba921342f
Christopher Long <93620761+cvalong@users.noreply.github.com>
2024-08-13 12:54:39 -0700

    feat: retrieve posts from fs and path

---
+++ src/lib/api.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import { join } from 'path';
+import matter from 'gray-matter';
+import { Post } from '@/interfaces/post';
+
+const postDirectory = join(process.cwd(), '_posts');
+
+export function getPostSlugs(): string[] {
+  return fs.readdirSync(postDirectory);
+}
+
+export function getPostBySlug(slug: string): Post {
+  const realSlug = slug.replace(/\.mdx$/, '');
+  const fullPath = join(postDirectory, `${realSlug}.mdx`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  const { data, content } = matter(fileContents);
+  const frontmatter = data as Omit<Post, 'slug' | 'content'>;
+
+  return {
+    ...frontmatter,
+    slug: realSlug,
+    content: content,
+  };
+}
+
+export function getAllPosts(): Post[] {
+  const slugs = getPostSlugs();
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug))
+    .sort((post1, post2) => (post1.publishedOn > post2.publishedOn ? -1 : 1));
+  return posts;
+}
