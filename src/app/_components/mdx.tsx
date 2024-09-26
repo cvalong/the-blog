d2c9a836b9d6a4fdf070e47df1c841d6924a2f0a
Christopher Long <93620761+cvalong@users.noreply.github.com>
2024-09-25 20:27:41 -0700

    feat: initial CustomMDX for each post

--- src/app/_components/mdx.tsx
+++ src/app/_components/mdx.tsx
@@ -1,7 +1,126 @@
-import type { MDXComponents } from 'mdx/types';
+import Link from 'next/link';
+import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc';
+import React from 'react';
 
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    ...components,
+interface CustomLinkProps
+  extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  children: React.ReactNode;
+}
+
+function CustomLink(props: CustomLinkProps) {
+  let href = props.href ?? '';
+
+  if (href.startsWith('/')) {
+    return (
+      <Link href={href} {...props}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  if (href.startsWith('#')) {
+    return <a {...props} />;
+  }
+
+  return <a target='_blank' rel='noopener noreferrer' {...props} />;
+}
+
+interface CodeProps extends React.HTMLAttributes<HTMLElement> {
+  children: Exclude<React.ReactNode, null | undefined>;
+}
+
+function Code({ children, ...props }: CodeProps) {
+  return <code dangerouslySetInnerHTML={{ __html: children }} {...props} />;
+}
+
+function stringifyChildren(children: React.ReactNode): string {
+  if (Array.isArray(children)) {
+    return children.map(stringifyChildren).join('-');
+  }
+
+  if (
+    typeof children === 'string' ||
+    typeof children === 'number' ||
+    typeof children === 'boolean'
+  ) {
+    return children.toString();
+  }
+
+  return '';
+}
+
+function slugify(str: string) {
+  return str
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, '-') // Replace spaces with -
+    .replace(/&/g, '-and-') // Replace & with 'and'
+    .replace(/[^\w\-]+/g, '') // Remove all non-word characters except for -
+    .replace(/\-\-+/g, '-'); // Replace multiple - with single -
+}
+
+function createHeading(level: number) {
+  interface HeadingProps {
+    children: React.ReactNode;
+  }
+
+  const Heading = ({ children }: HeadingProps): React.JSX.Element => {
+    let slug = slugify(stringifyChildren(children));
+
+    let textSize = '';
+    switch (level) {
+      case 1:
+        textSize = 'text-3xl';
+        break;
+      case 2:
+        textSize = 'text-2xl';
+        break;
+      case 3:
+        textSize = 'text-xl';
+        break;
+      default:
+        textSize = 'text-lg';
+    }
+
+    let hClass = textSize + ' font-medium tracking-tight mt-6 mb-2';
+    return React.createElement(
+      `h${level}`,
+      { id: slug, className: hClass },
+      [
+        React.createElement('a', {
+          href: `#${slug}`,
+          key: `link-${slug}`,
+          className:
+            'ml-[-1rem] pr-2 w-4/5 max-w-3xl absolute cursor no-underline hover:underline invisible hover:visible after:content-["#"] after:text-neutral-300 dark:text-neutral-700',
+        }),
+      ],
+      children
+    );
   };
+
+  return Heading;
+}
+
+let components = {
+  h1: createHeading(1),
+  h2: createHeading(2),
+  h3: createHeading(3),
+  h4: createHeading(4),
+  h5: createHeading(5),
+  h6: createHeading(6),
+  a: CustomLink,
+  code: Code,
+};
+
+interface CustomMDXProps extends MDXRemoteProps {
+  components?: Record<string, React.ReactElement> & typeof components;
+}
+export function CustomMDX(props: CustomMDXProps) {
+  return (
+    <MDXRemote
+      {...props}
+      components={{ ...components, ...(props.components || {}) }}
+    />
+  );
 }
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { CustomMDX } from '@/app/_components/mdx';
 import { getAllPosts, getPostBySlug } from '@/app/blog/utils';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
@@ -15,14 +16,18 @@ export default function Post({ params }: Params) {
     <main className='bg-[#f3f0e8] dark:bg-[#18251D] flex flex-col justify-start md:grid md:grid-cols-[1fr_640px_1fr]'>
       <div className='space-y-4 py-10 px-6 md:col-start-2'>
         <div className='flex flex-col gap-4'>
-          <h2>{title}</h2>
-          <p>{content}</p>
-          {Object.entries(rest).map(([key, value], index) => (
-            <div key={index}>
-              <span>{key}: </span>
-              <span>{value}</span>
-            </div>
-          ))}
+          <h1 className='font-semibold text-3xl tracking-tighter'>{title}</h1>
+          <article className='prose prose-stone dark:prose-invert md:prose-lg lg:prose-xl prose-headings:underline prose-a:text-blue-600'>
+            <CustomMDX source={content} />
+          </article>
+          <section className='space-y-4 py-10 px-6'>
+            {Object.entries(rest).map(([key, value], index) => (
+              <div key={index}>
+                <span>{key}: </span>
+                <span>{value}</span>
+              </div>
+            ))}
+          </section>
         </div>
       </div>
     </main>
