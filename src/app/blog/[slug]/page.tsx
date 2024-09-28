f451d6014c3a16b67d268eb5b6df074fea68478d
Christopher Long <93620761+cvalong@users.noreply.github.com>
2024-09-27 18:03:02 -0700

    fix: separate post data into component for easy removal

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,6 +2,11 @@ import { CustomMDX } from '@/app/_components/mdx';
 import { getAllPosts, getPostBySlug } from '@/app/blog/utils';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import { type Post } from '../interfaces/post';
+
+interface Params {
+  params: { slug: string };
+}
 
 export default function Post({ params }: Params) {
   const post = getPostBySlug(params.slug) ?? {};
@@ -15,26 +20,15 @@ export default function Post({ params }: Params) {
   return (
     <main className='bg-[#f3f0e8] dark:bg-[#18251D] flex flex-col justify-start md:grid md:grid-cols-[1fr_640px_1fr]'>
       <div className='space-y-4 py-10 px-6 md:col-start-2'>
-        <div className='flex flex-col gap-4'>
-          <h1 className='font-semibold text-3xl tracking-tighter'>{title}</h1>
-          <article className='prose prose-stone dark:prose-invert md:prose-lg lg:prose-xl prose-headings:underline prose-a:text-blue-600'>
-            <CustomMDX source={content} />
-          </article>
-          <section className='space-y-4 py-10 px-6'>
-            {Object.entries(rest).map(([key, value], index) => (
-              <div key={index}>
-                <span>{key}: </span>
-                <span>{value}</span>
-              </div>
-            ))}
-          </section>
-        </div>
+        <h1 className='font-semibold text-3xl tracking-tighter'>{title}</h1>
+        <article className='prose prose-stone dark:prose-invert md:prose-lg lg:prose-xl prose-headings:underline prose-a:text-blue-600'>
+          <CustomMDX source={content} />
+        </article>
+        <PostData partialPost={rest} />
       </div>
     </main>
   );
 }
-
-type Params = { params: { slug: string } };
 
 export function generateMetadata({ params }: Params): Metadata {
   const post = getPostBySlug(params.slug);
@@ -59,4 +53,21 @@ export async function generateStaticParams() {
   return posts.map((post) => ({
     slug: post.slug,
   }));
+}
+
+function PostData({
+  partialPost,
+}: {
+  partialPost: Omit<Post, 'title' | 'content' | 'slug'>;
+}) {
+  return (
+    <section className='space-y-4 px-4'>
+      {Object.entries(partialPost).map(([key, value], index) => (
+        <div key={index}>
+          <span>{key}: </span>
+          <span>{value}</span>
+        </div>
+      ))}
+    </section>
+  );
 }
